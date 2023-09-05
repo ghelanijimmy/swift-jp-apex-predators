3f5e6e98ae7fb882d95e61ad4eb52441ff306667
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-05 08:33:27 -0400

    Initial Commit

---
+++ JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by Jimmy Ghelani on 2023-09-05.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
